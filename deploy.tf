3d85bbed2d831e0db7f1679de04c9301ba0b3a3b
Fabio Andrei Lorentz <tifabio@gmail.com>
2021-06-29 12:23:40 -0300

    Terraform deploy using EC2

--- deploy.tf
+++ deploy.tf
@@ -1,25 +1,9 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
 provider "aws" {
   region = "us-east-1"
 }
 
 resource "aws_instance" "web" {
-  ami                    = data.aws_ami.ubuntu.id
+  ami                    = "ami-0dd76f917833aac4b"
   instance_type          = "t2.micro"
   vpc_security_group_ids = ["${aws_security_group.web.id}"]
   user_data              = "${file("deploy.sh")}"
@@ -31,14 +15,6 @@ resource "aws_security_group" "web" {
     to_port     = 3001
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "SSH"
-    from_port   = 22 # SSH client port is not a fixed port
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] #allow web traffic. 46.64.73.251/32
   }
 
   egress {
